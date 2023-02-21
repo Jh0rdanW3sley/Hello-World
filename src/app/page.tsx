a4f35b09ea8b746cb7b8f521710299781f3ba178
jhordan wesley <jhordan.pastorello@ampernet.com.br>
2023-02-21 19:48:26 -0300

    Botoões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,19 @@ export default function Home() {
   <main>
 
     <div className='hello-world'>
+
+
+    <div className='social'>
+      <div className='bot git'><a href="https://github.com/Jh0rdanW3sley"><h2>Github</h2></a></div>
+      <div className='bot link'><a href="https://www.linkedin.com/in/jhordan-wesley-291a55245/"><h2>Linkedin</h2></a></div>
+    </div>
+
         <div className='logo'>
         <h2>Jh</h2>
         </div>
       <h1>Hello World!</h1>
     </div>
+    
   </main>
   
   )
