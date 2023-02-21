2eef2f531108da19fa49f497d72e072a2520c9e0
jhordan wesley <jhordan.pastorello@ampernet.com.br>
2023-02-21 18:39:23 -0300

    Atualização de Divs e CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,21 @@ import './style.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+
+
+
 export default function Home() {
   return (
   <main>
-    <div className='logo'>
-      <h2>Jh</h2>
+
+    <div className='hello-world'>
+        <div className='logo'>
+        <h2>Jh</h2>
+        </div>
+      <h1>Hello World!</h1>
     </div>
-    <h1>Hello World!</h1>
   </main>
+  
   )
 }
